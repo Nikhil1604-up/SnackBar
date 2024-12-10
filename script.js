ceafabcabc46c6bea9a3af361cfdb1891f2d046c
Nikhil upadhyay <nikhilupadhyay1604@gmail.com>
2024-12-10 07:14:19 +0530

    add: added animation and timer to progress bar for each toast type

--- script.js
+++ script.js
@@ -11,15 +11,22 @@ function showToast(type) {
     case "success":
       toast.innerHTML =
         '<i class="fa-solid fa-circle-check" style="color:green"></i> Successfully submitted';
+      toast.classList.add("success");
       break;
     case "error":
       toast.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color:red"></i> Erorr Occured!`;
+      toast.classList.add("error");
       break;
     default:
       toast.innerHTML = `<i class="fa-solid fa-circle-exclamation" style="color:orange"></i> Invalid Action!`;
+      toast.classList.add("invalid");
   }
 
   // Appending this success toast inside the toastBox
 
   toastBox.appendChild(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 6000);
 }
